c2278db22da38658549ce887b437bf3e67a1b4c0
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2020-11-15 14:50:49 -0800

    cleanup readme

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Scripts
-
 ### `yarn start`
 
 Runs the app in the development mode.\
@@ -23,3 +22,14 @@ Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
 
 The build is minified and the filenames include the hashes.
+
+## API
+
+This app leverages the Jobcoin API. [See Jobcoin docs for details.](https://jobcoin.gemini.com/errant-clear/api#addresses-address-info).
+
+## Usage
+1. Login with a "jobcoin address" created by the [jobcoin management tool](https://jobcoin.gemini.com/errant-clear).
+2. Once logged in you can: 
+- see current balance 
+- view history of all addresses 
+- send coin from your address to another address
--- src/components/card.js
+++ src/components/card.js
@@ -16,38 +16,22 @@ export const Card = ({ headerText, children }) => {
 }
 
 const CardStyle = styled.div`
-    border: 1px solid ${primary};
-    border-radius: 5px;
-    margin-bottom: 20px;
-    .header {
-      font-size: medium;
-      text-align: center;
-      height: 60px;
-      border-bottom: 1px solid ${primary};
-      display:flex;
-      justify-content: center;
-      align-items: center;
-      padding: 1em;
-    }
-    .body {
-      justify-content: center;
-      align-items: center;
-      margin: 50px;
-    }
-    form {
-      display: flex;
-      flex-direction: column;
-      label {
-        width: 100%;
-        display: block;
-        font-size: small;
-      }
-      input {
-        height: 30px;
-        border: 1px solid ${primary};
-        border-radius: 3px;
-        margin-bottom: 18px;
-        margin-top: 5px;
-      }
-    }
+  border: 1px solid ${primary};
+  border-radius: 5px;
+  margin-bottom: 20px;
+  .header {
+    font-size: medium;
+    text-align: center;
+    height: 60px;
+    border-bottom: 1px solid ${primary};
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+  }
+  .body {
+    justify-content: center;
+    align-items: center;
+    margin: 50px;
+  }
 `
